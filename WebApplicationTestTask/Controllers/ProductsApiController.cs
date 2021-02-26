aa0a47108c5841aecc66d961291cb18ff8a6cc93
kyrylok2908 <kirilukraine@gmail.com>
2021-02-26 13:25:40 +0200

    Add project files.

---
+++ WebApplicationTestTask/Controllers/ProductsApiController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationTestTask.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsApiController : ControllerBase
+    {
+
+    }
+}
